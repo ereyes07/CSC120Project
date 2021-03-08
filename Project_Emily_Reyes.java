e9d922201066639278c3a49e71bbdf12a3da6ef7
Emily Reyes <70026352+ereyes07@users.noreply.github.com>
2021-03-08 18:00:52 -0500

    Create Project_Emily_Reyes.java
    Added code

---
+++ Project_Emily_Reyes.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Project_Emily_Reyes
+{
+   public static void main(String[] args)
+   {
+      double graphic = 0.0;
+      double processor = 0.0;
+      double cores = 0.0;
+      int choice = 0;
+      
+      Scanner keyboard = new Scanner (System.in);
+      
+      System.out.println("Please enter the clock speed (in megahertz) of your processor(CPU).");
+      graphic = keyboard.nextDouble();
+      
+      System.out.println("Please enter the number of cores that your processor (CPU) has. ");
+      processor = keyboard.nextDouble();
+      
+      System.out.println("Please select the resolution of your monitor");
+      
+      System.out.println("1. 1280 x 720");
+      System.out.println("2. 1920 x 1080");
+      System.out.println("3. 2560 x 1440");
+      System.out.println("4. 3840 x 2160");
+      choice = keyboard.nextInt();
+      
+      
+
+      
+      
+      
+    
+   
+   
+   }
+}      
